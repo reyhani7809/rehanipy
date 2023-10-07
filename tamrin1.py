87fa950d77806ab6d7852b930d89f173a23c7b96
reyhani7809 <j.reyhani7809@gmail.com>
2023-10-07 22:04:17 +0330

    tamrin avval

---
+++ tamrin1.py
@@ -0,0 +1,23 @@
+widte=int(input())
+length=int(input())
+area=(widte*length)
+print(area)
+EnvironmentError=(widte+length)*2
+print(EnvironmentError)
+
+radius=int(input())
+area=(radius*radius*3/14)
+print(area)
+ 
+EnvironmentError=(2*radius*3/14)
+print(EnvironmentError)
+Name=input()
+family=input()
+old=int(input())
+city=input()
+print("hello i am",Name,family,old,city)
+
+hour=int(input())
+minute=hour/60
+secont=minute/60
+print(hour,minute,secont)
