a099d96092ee68bc214a1f2c54e848348333df0e
reyhani7809 <147168175+reyhani7809@users.noreply.github.com>
2023-10-18 22:02:20 +0330

    Add files via upload

---
+++ jalase 2 3.py
@@ -0,0 +1,11 @@
+average = int(input())
+
+if average >= 17:
+    print("دانشجوی ممتاز")
+
+if 12 <= average < 17:
+    print("دانشجوی عادی")
+
+if average < 12:
+    print("دانشجوی ضعیف")   
+
---
+++ jalase 2 4.py
@@ -0,0 +1,10 @@
+vazn = int(input())
+ghad = int(input())
+
+BMI = vazn/ghad
+
+if 20 <= BMI > 30:
+    print("OK") 
+else:
+    print("NOT OK")
+
